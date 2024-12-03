860fc8170a8e9090e3c712b2bd301480c1bed76b
Alexander Dow <57762555+Glythium@users.noreply.github.com>
2024-12-03 00:02:15 -0600

    Day1/two (#3)
    * Rename input file, add solution for second puzzle
    
    * Finish day 2

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ The main driver for AoC 2024. Run this from the src directory
 """
 
 try:
-    from classes.d_1 import D1
+    from classes.d_2 import D2
 except ModuleNotFoundError:
     print("[!] Could not find modules!")
 
@@ -11,10 +11,9 @@ def main():
     """
     Generates a solution for a day's puzzles
     """
-    print("[*] Beginning...")
-    d1 = D1(debug=True)
-    print(d1.one("../input/d1.txt"))
-    print(d1.two("../input/d1.txt"))
+    d2 = D2(1, 3, debug=True)
+    print(d2.solve("../input/d2.txt"))
+    print(d2.solve("../input/d2.txt", should_dampen=True))
 
 
 if __name__ == '__main__':
