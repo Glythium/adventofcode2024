c49dfde317df5cd8ecfb0593095e678a8cff8db0
Alex Dow <alexdow1991@gmail.com>
2024-12-04 00:16:27 -0600

    I'm getting and answer that works for someone else, I've double-checked the input, my result is 'too low'

--- src/classes/d_4.py
+++ src/classes/d_4.py
@@ -46,38 +46,48 @@ class D4(Day):
                         # Now, we need to start reaching out from here to
                         #   find the rest of our 'MAS'
 
-                        # Forward horizontal
+                        # Forward horizontal (+X , Y)
                         if line[idx_x + 1] + line[idx_x + 2] + line[idx_x + 3] == 'MAS':
                             self.total += 1
                         
-                        # Straight down vertically
+                        # Straight down vertically (X , +Y)
                         if self.matrix[idx_y + 1][idx_x] + self.matrix[idx_y + 2][idx_x] + self.matrix[idx_y + 3][idx_x] == "MAS":
                             self.total += 1
                         
                         # For the reverse searches, we need to check to ensure
                         #   we don't attempt a negative index which will wrap
                         if idx_x - len("MAS") >= 0:
-                            # Reverse horizontal
+                            # Reverse horizontal (-X , Y)
                             if line[idx_x - 1] + line[idx_x - 2] + line[idx_x - 3] == "MAS":
+                                self.total += 1
+                            
+                            # Reverse down diagonally (-X , +Y)
+                            if self.matrix[idx_y + 1][idx_x - 1] + self.matrix[idx_y + 2][idx_x - 2] + self.matrix[idx_y + 3][idx_x - 3] == "MAS":
                                 self.total += 1
                         
                         if idx_y - len("MAS") >= 0:
-                            # Reverse up vertically
+                            # Reverse up vertically (X , -Y)
                             if self.matrix[idx_y - 1][idx_x] + self.matrix[idx_y - 2][idx_x] + self.matrix[idx_y - 3][idx_x] == "MAS":
                                 self.total += 1
                             
+                            # Forward up diagonally (+X , -Y)
+                            if self.matrix[idx_y - 1][idx_x + 1] + self.matrix[idx_y - 2][idx_x + 2] + self.matrix[idx_y - 3][idx_x + 3] == "MAS":
+                                self.total += 1
+                            
+                            # Forward down diagonally (+X , +Y)
+                            if self.matrix[idx_y + 1][idx_x + 1] + self.matrix[idx_y + 2][idx_x + 2] + self.matrix[idx_y + 3][idx_x + 3] == "MAS":
+                                self.total += 1
+                        
+                        if idx_x - len("MAS") >= 0 and idx_y - len("MAS") >= 0:
+                            # Reverse up diagonally (-X , -Y)
+                            if self.matrix[idx_y - 1][idx_x - 1] + self.matrix[idx_y - 2][idx_x - 2] + self.matrix[idx_y - 3][idx_x - 3] == "MAS":
+                                self.total += 1
                     except IndexError:
                         # This Exception will catch attempts to overextend
                         #   the limit of our matrix. However, Python does
                         #   allow for negative indexing, so we handle
                         #   that in the try block.
                         pass
-                        
-        # print(self.matrix[3][3])
-        # print(self.matrix[2][4])
-        # print(self.matrix[1][5])
-        # print(self.matrix[0][6])
-
         return self.total
 
     def two(self):
